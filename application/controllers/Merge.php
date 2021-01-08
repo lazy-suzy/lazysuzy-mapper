1dac336b65872b8f971391eef1bacd8686c6e7a5
Aditya <adityasaxena602@gmail.com>
2021-01-08 15:26:31 +0530

    add new merge cntroller for merge logic init

--- application/controllers/Merge.php
+++ application/controllers/Merge.php
@@ -8,18 +8,23 @@ class Merge extends CI_Controller
 {
     private $master_data_table = "master_data";
 
-    public function remove_zip_code_from_online_msg()
+    public function remove_ship_code_from_online_msg()
     {
 
         $rows = $this->db->select(['id', 'online_msg'])->from($this->master_data_table)->get()->result();
         $replace_str = "to ZIP code";
+        $replace_str2 = "for ZIP cde";
         echo "Total Rows in ", $this->master_data_table, " : ", sizeof($rows), "\n";
         $updates_done = 0;
         foreach ($rows as $row) {
             $id = $row->id;
             $online_msg = $row->online_msg;
-            if (strlen($online_msg) > 0 && strpos($online_msg, $replace_str) !== false) {
-                $online_msg = str_replace($replace_str, "", $online_msg);
+            if (
+                strlen($online_msg) > 0 &&
+                (strpos($online_msg, $replace_str) !== false
+                    || strpos($online_msg, $replace_str2) !== false)
+            ) {
+                $online_msg = str_replace([$replace_str, $replace_str2], "", $online_msg);
                 if ($this->db->set('online_msg', $online_msg)->where('id', $id)->update($this->master_data_table))
                     $updates_done++;
             }
