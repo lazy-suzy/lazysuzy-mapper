12e5333ac86ef0d7dc5cb6da773aa0d850ba746d
Aditya <adityasaxena602@gmail.com>
2021-03-24 14:37:19 +0530

    correct image path for nw variations

--- application/controllers/nw-data-save.php
+++ application/controllers/nw-data-save.php
@@ -206,8 +206,8 @@ function save_product($product)
                     $var['swatch'] = multiple_download([$var['swatch']], '/var/www/html/nw/new-09062020');
                 }
                 
-                $str = "INSERT INTO nw_variations (product_id, sku, price, was_price, attribute_1, attribute_2, attribute_3, attribute_4, attribute_5, attribute_6, `image`, swatch_image_path, status ) VALUES (
-                    '{$var['product_sku']}', '{$var['variation_sku']}', '{$var['min_price']}', '{$var['was_price']}' , '{$var['attribute_1']}', '{$var['attribute_2']}', '{$var['attribute_3']}', '{$var['attribute_4']}', '{$var['attribute_5']}', '{$var['attribute_6']}', '$img_v', '{$var['swatch']}', '{$var['product_status']}') ON DUPLICATE KEY UPDATE price = '{$var['min_price']}'";
+                $str = "INSERT INTO nw_variations (product_id, sku, price, was_price, attribute_1, attribute_2, attribute_3, attribute_4, attribute_5, attribute_6, `image`, image_path ,swatch_image_path, status ) VALUES (
+                    '{$var['product_sku']}', '{$var['variation_sku']}', '{$var['min_price']}', '{$var['was_price']}' , '{$var['attribute_1']}', '{$var['attribute_2']}', '{$var['attribute_3']}', '{$var['attribute_4']}', '{$var['attribute_5']}', '{$var['attribute_6']}', '$img_v', '$img_v' , '{$var['swatch']}', '{$var['product_status']}') ON DUPLICATE KEY UPDATE price = '{$var['min_price']}'";
                 if (!mysqli_query($conn, $str)) {
                     echo $str;
                     die('variation not saved ' . mysqli_error($conn));
